d7202d261b2772a7ced1fe4bc8ad99ed963f3afb
mallikarjuna <mallienturi@gmail.com>
2026-07-09 12:07:05 +0530

    Overwrite log file on each test run

--- utils/logger.py
+++ utils/logger.py
@@ -7,7 +7,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
     handlers=[
-        logging.FileHandler("logs/api.log"),
+        logging.FileHandler("logs/api.log",mode="w"),
         logging.StreamHandler()
     ]
 )
